e573ae97dd9c35e4862fd6f780db3a6bbe5f4795
Iain Samuel McLean Elder <iain@isme.es>
2021-12-29 15:57:53 +0100

    Label plots with profile names

--- plot.py
+++ plot.py
@@ -1,15 +1,21 @@
 # type: ignore
 
 from glob import glob
+import os
+import sys
 
 import matplotlib.pyplot as plt
 import pandas as pd
+
+output_dir = sys.argv[1]
+
+os.chdir(output_dir)
 
 profiles = glob("*CoveRunner.csv")
 
 ax = None
 for p in profiles:
     df = pd.read_csv(p)
-    ax = df.plot(ax=ax, x="CPU_Time", y="Resident_Memory_size")
+    ax = df.plot(ax=ax, x="CPU_Time", y="Resident_Memory_size", label=p)
 
 plt.show()
