70b28291a8f721f37daaa3bc2444c6fc336a0f56
Iain Samuel McLean Elder <iain@isme.es>
2023-07-01 18:10:35 +0200

    Call regional API in region tests

--- tests/test_regions.py
+++ tests/test_regions.py
@@ -1,77 +1,82 @@
+from typing import Optional
+
 import pytest
 from boto3.session import Session
 
 from botocove import cove
-from tests.moto_mock_org.moto_models import SmallOrg
+from tests.moto_mock_org.moto_models import SmallOrg 
+
+
+pytestmark = pytest.mark.parametrize(
+    "assuming_session", [None]
+    # "assuming_session", [None, Session(), Session(region_name="eu-west-1")]
+)
+
+
+def _call_regional_api(session: Session) -> str:
+    if session.client("ec2").describe_availability_zones():
+        return "OK"
 
 
 def test_when_region_is_unspecified_then_result_has_no_region_key(
-    mock_small_org: SmallOrg,
+    mock_small_org: SmallOrg, assuming_session: Optional[Session]
 ) -> None:
-    @cove()
-    def do_nothing(session: Session) -> None:
-        pass
+    call_regional_api = cove(_call_regional_api)
 
-    output = do_nothing()
+    output = call_regional_api()
     assert output["Results"]
     for result in output["Results"]:
         assert "Region" not in result
 
 
 def test_when_region_is_unspecified_then_output_has_one_result_per_account(
-    mock_session: Session, mock_small_org: SmallOrg
+    mock_session: Session, mock_small_org: SmallOrg, assuming_session: Optional[Session]
 ) -> None:
-    @cove()
-    def do_nothing(session: Session) -> None:
-        pass
+    call_regional_api = cove(_call_regional_api)
 
-    output = do_nothing()
+    output = call_regional_api()
     assert len(output["Results"]) == _count_member_accounts(mock_session)
 
 
-def test_when_region_is_str_then_raises_type_error(mock_small_org: SmallOrg) -> None:
-    @cove(regions="eu-west-1")  # type: ignore[arg-type]
-    def do_nothing() -> None:
-        pass
+def test_when_region_is_str_then_raises_type_error(
+    mock_small_org: SmallOrg, assuming_session: Optional[Session]
+) -> None:
+    call_regional_api = cove(_call_regional_api, regions="eu-west-1")  # type: ignore[arg-type]
 
     with pytest.raises(
         TypeError, match=r"regions must be a list of str\. Got str 'eu-west-1'\."
     ):
-        do_nothing()
+        call_regional_api()
 
 
-def test_when_region_is_empty_then_raises_value_error(mock_small_org: SmallOrg) -> None:
-    @cove(regions=[])
-    def do_nothing() -> None:
-        pass
+def test_when_region_is_empty_then_raises_value_error(
+    mock_small_org: SmallOrg, assuming_session: Optional[Session]
+) -> None:
+    call_regional_api = cove(_call_regional_api, regions=[])
 
     with pytest.raises(
         ValueError, match=r"regions must have at least 1 element\. Got \[\]\."
     ):
-        do_nothing()
+        call_regional_api()
 
 
 def test_when_any_region_is_passed_then_result_has_region_key(
-    mock_small_org: SmallOrg,
+    mock_small_org: SmallOrg, assuming_session: Optional[Session]
 ) -> None:
-    @cove(regions=["eu-west-1"])
-    def do_nothing(session: Session) -> None:
-        pass
+    call_regional_api = cove(_call_regional_api, regions=["eu-west-1"])
 
-    output = do_nothing()
+    output = call_regional_api()
     assert output["Results"]
     for result in output["Results"]:
         assert result["Region"] == "eu-west-1"
 
 
 def test_when_two_regions_are_passed_then_output_has_one_result_per_account_per_region(
-    mock_session: Session, mock_small_org: SmallOrg
+    mock_session: Session, mock_small_org: SmallOrg, assuming_session: Optional[Session]
 ) -> None:
-    @cove(regions=["eu-west-1", "us-east-1"])
-    def do_nothing(session: Session) -> None:
-        pass
+    call_regional_api = cove(_call_regional_api, regions=["eu-west-1", "us-east-1"])
 
-    output = do_nothing()
+    output = call_regional_api()
 
     number_of_member_accounts = _count_member_accounts(mock_session)
 
@@ -80,6 +85,7 @@ def test_when_two_regions_are_passed_then_output_has_one_result_per_account_per_
             1 for result in output["Results"] if result["Region"] == region
         )
         assert number_of_results_per_region == number_of_member_accounts
+
 
 
 def _count_member_accounts(session: Session) -> int:
